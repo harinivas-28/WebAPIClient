5c76edbc7f00c4395f910aafd219677b37e34d07
Harinivas.Ganjarla <Harinivas.Ganjarla@IVNT-7C0LFK3>
2025-08-07 14:26:25 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Net.Http.Headers;
+
+using HttpClient client = new();
+client.DefaultRequestHeaders.Accept.Clear();
+client.DefaultRequestHeaders.Accept.Add(
+    new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
+client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
+
+await ProcessRepoAsync(client);
+static async Task ProcessRepoAsync(HttpClient client)
+{
+    var json = await client.GetStringAsync("https://api.github.com/orgs/dotnet/repos");
+    Console.WriteLine(json);
+}
+
+// JsonSerializer = class used to deserialize JSON into C# objects
